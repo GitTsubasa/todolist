df1209a0abafb7122997ecf6188ad10880f95dfc
pwang10 <pwang11@binghamton.edu>
2024-03-15 23:47:29 -0400

    delete build

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
 
   output: {
     path: path.join(__dirname, '/dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    clean: true
   },
 
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
